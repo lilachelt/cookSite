d0cf7b4c261a26d02b3c8531bf79eb912ac37a1b
Nativ Atir <nativatir@gmail.com>
2016-08-04 09:00:05 +0300

    show data to website

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.set('view engine', 'hbs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -35,102 +35,96 @@ app.use('/', routes);
 app.use('/users', users);
 app.use('/search', search);
 
-MongoClient.connect("mongodb://Vmedu94.mtacloud.co.il:27017/cook", function(err, db) {
+MongoClient.connect("mongodb://Vmedu94.mtacloud.co.il:27017/cook", function (err, db) {
 
-  if(err) throw err;
+    if (err) throw err;
 
-  assert.equal(null, err);
+    assert.equal(null, err);
 
-  console.log("Successfully connected to MongoDB.");
+    console.log("Successfully connected to MongoDB.");
 
-  app.post('/', function(req, res, next) {
-    var searchString = req.body.search;
+    app.post('/', function (req, res, next) {
+        var searchString = req.body.search;
 
-      //TODO delete links after
-    if (searchString != '') {
-        // next(Error('Please insert search string!'));
+        //TODO delete links after
+        if (searchString != '') {
+            // next(Error('Please insert search string!'));
 
-        /**
-         * Show the search key word in the search box after 'search' button clicked.
-         */
-        req.body.search = searchString;
+            /**
+             * Show the search key word in the search box after 'search' button clicked.
+             */
+            req.body.search = searchString;
 
-        db.collection('SearchStrings').find({'StringSearch': searchString}).toArray(function (err, docs) {
+            db.collection('SearchStrings').find({'StringSearch': searchString}).toArray(function (err, docs) {
 
-            if (err) throw err;
-            if (docs.length < 1) {
-                console.dir("No documents found.");
-                //goto robot
-            } else {
-                console.dir("Documents found!");
-                for (var doc in docs) {
-                    var linksId = docs[doc]["Links"];
+                if (err) throw err;
+                if (docs.length < 1) {
+                    console.dir("No documents found.");
+                    //goto robot
+                } else {
+                    console.dir("Documents found!");
+                    for (var doc in docs) {
+                        var linksId = docs[doc]["Links"];
+                    }
                 }
-            }
 
                 /**
                  * get all the titles using links ID from collection 'Links'
                  */
-                getDataFromDbUsingLinksId(linksId, db,'Links','Title', '_id' ,function(titlesUrlsArr) {
-                         // console.log(titlesUrlsArr);
-                    getDataFromDbUsingLinksId(linksId, db,'Links','Link', '_id', function(linksUrlsArr){
-                          //  console.log(linksUrlsArr);
-                        getDataFromDbUsingLinksId(linksId, db,'Links','Ingredients', '_id', function(IngredientsIDArr){
-                                //console.log(IngredientsIDArr);
+                getDataFromDbUsingLinksId(linksId, db, 'Links', 'Title', '_id', function(titlesUrlsArr) {
 
-                            getDataFromDbUsingLinksId(IngredientsIDArr, db,'LinksToWords','Words','_id',function(IDsIngredientsArr) {
-                                    //console.log(IDsIngredientsArr);
-                                buildIngredientsNamesArray(db, IDsIngredientsArr, function(ingredientsNames) {
-                                   // console.log(ingredientsNames);
+                    getDataFromDbUsingLinksId(linksId, db, 'Links', 'Link', '_id', function(linksUrlsArr) {
+
+                        getDataFromDbUsingLinksId(linksId, db, 'Links', 'Ingredients', '_id', function(IngredientsIDArr) {
+
+                            getDataFromDbUsingLinksId(IngredientsIDArr, db, 'LinksToWords', 'Words', '_id', function(IDsIngredientsArr) {
+
+                                buildIngredientsNamesArray(db, IDsIngredientsArr, function (ingredientsNames) {
+
                                     mergeArraysToOneArray(db, titlesUrlsArr, linksUrlsArr, ingredientsNames, function(arrayDataResult) {
-                                        console.log('$' + arrayDataResult);
-                                        console.log();
-
+                                        res.render('search', {arrayDataResult: arrayDataResult});
                                     });
-
-                                    });
-
                                 });
-
-                             });
+                            });
                         });
-                                //res.render('search', {searchKeyWord: searchString, titleResults: titlesUrlsArr});\
                     });
                 });
+
+            });
         }
-  });
+    });
 
-   // catch 404 and forward to error handler
-  app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-  });
+    // catch 404 and forward to error handler
+    app.use(function (req, res, next) {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    });
 
-});
+}); //End MongoClient connect.
 
 // error handlers
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 module.exports = app;
@@ -142,22 +136,22 @@ function getDataFromDbUsingLinksId(linksId, db, collectionName, itemName, search
     var query = {};
 
     if (searchParam == '_id') {
-        linksId.forEach(function(id){
+        linksId.forEach(function (id) {
             mongoIds.push(new ObjectID(id));
         });
     } else {
         mongoIds = linksId;
     }
 
-    query[searchParam] = { $in: mongoIds };
+    query[searchParam] = {$in: mongoIds};
 
-    db.collection (collectionName).find(query).toArray(function (err,doc) {
+    db.collection(collectionName).find(query).toArray(function (err, doc) {
         if (err) throw err;
-        arrDataLinks =  _.map (doc, function(item)
-            {
-                return item[itemName]
-            }
-        );
+
+        arrDataLinks = _.map(doc, function (item) {
+            return item[itemName]
+        });
+
         onResults(arrDataLinks);
     });
 
@@ -166,29 +160,35 @@ function getDataFromDbUsingLinksId(linksId, db, collectionName, itemName, search
 
 
 function buildIngredientsNamesArray(db, ingredientsIdArr, callback) {
-    ingredientsNamesArr = [];
 
-    ingredientsIdArr.forEach(function(entry) {
-        getDataFromDbUsingLinksId(entry, db, 'Ingredients', 'Word', '_id', function(ingredientsNames) {
-            console.log(ingredientsNames);
+    ingredientsNamesArr = [];
+    counter = 0;
+    ingredientsIdArrLength = ingredientsIdArr.length;
+
+    for (var i = 0; i < ingredientsIdArrLength; i++) {
+
+        getDataFromDbUsingLinksId(ingredientsIdArr[i], db, 'Ingredients', 'Word', '_id', function (ingredientsNames) {
+            counter++;
+
             ingredientsNamesArr.push(ingredientsNames);
-            callback(ingredientsNamesArr);
+
+            if (counter === ingredientsIdArrLength) {
+                callback(ingredientsNamesArr);
+            }
         });
-    });
-    return ingredientsNamesArr;
+    }
 }
 
 
-
-function mergeArraysToOneArray(db, titlesUrlsArr, linksUrlsArr, ingredientsNamesArr, callback)
-{
+function mergeArraysToOneArray(db, titlesUrlsArr, linksUrlsArr, ingredientsNamesArr, callback) {
     var arrResult = new Array(linksUrlsArr.length);
 
-    for (var i=0; i < arrResult.length; i++) {
+    for (var i = 0; i < arrResult.length; i++) {
         arrResult[i] = new Array(3);
         arrResult[i][0] = titlesUrlsArr[i];
         arrResult[i][1] = linksUrlsArr[i];
         arrResult[i][2] = ingredientsNamesArr[i];
     }
+
     callback(arrResult);
 }
