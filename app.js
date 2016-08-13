3622e9304eada5d6cc1319f5f5dd9fb3e176807d
mor.schwartz <moryifrach@gmail.com>
2016-08-14 00:12:38 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app.js
    
    עדכון הוספת מילים שלא בDB לתוך התור והצכת דף שגיאה כאשר אין מתכון להציג

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.use('/autocomplete',autocomplete);
     };
   })
 
-
+//////////////////////////
    // catch 404 and forward to error handler
   app.use(function(req, res, next) {
     var err = new Error('Not Found');
